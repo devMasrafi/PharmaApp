76356889140560e10e0976288232321f3fdcd48b
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-12-11 21:33:24 +0600

    admin creation and userModel

---
+++ backEnd/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+
+// user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum:['admin', 'staff'],
+        default: 'staff',
+    },
+    name: {
+        type: String,
+    },
+    address: {
+        type: String,
+    }
+},{
+    timestamps: true,
+})
+
+
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+
+module.exports = mongoose.model("User", userSchema);
