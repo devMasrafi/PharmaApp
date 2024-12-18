625c6bc274b46e71fbe1c98eb2c3f23ccdb73878
km-saifullah <kmsaifullah16@gmail.com>
2024-12-18 16:26:02 +0600

    restrict normal user to create a user feature added

--- backEnd/routes/authRoutes.js
+++ backEnd/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const auth = require("../controllers/authController");
 const router = express.Router();
 
-router.route("/").post(auth.signup);
+router.route("/").post(auth.protect, auth.adminAuth, auth.signup);
 router.route("/login").post(auth.login);
 
 module.exports = router;
