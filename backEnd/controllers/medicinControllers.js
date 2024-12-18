ad8a3e57dec56e1848e25a2a5ace361ca7557b4d
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-12-18 13:40:25 +0600

    controllers for medicine

---
+++ backEnd/controllers/medicinControllers.js
@@ -0,0 +1,78 @@
+const Medicine = require("../models/medicineModel");
+
+const addMedicine = async (req, res) => {
+  try {
+    const {
+      name,
+      manufacturer,
+      illnesses,
+      stock,
+      buyPrice,
+      sellPrice,
+      sellerInfo,
+    } = req.body;
+
+    const newMedicine = new Medicine({
+      name,
+      manufacturer,
+      illnesses,
+      stock,
+      buyPrice,
+      sellPrice,
+      sellerInfo,
+    });
+
+    await newMedicine.save();
+    res.status(201).json(newMedicine);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+// get all medicine
+const getMedicine = async (req, res) => {
+  try {
+    const medicines = await Medicine.find();
+    res.json(medicines);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+// upupdateMedicine
+
+const updateMedicine = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const updatedMedicine = await Medicine.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    if (!updatedMedicine) {
+      return res.json({ message: "medicine not found" });
+    }
+
+    res.json(updatedMedicine);
+  } catch (error) {
+    res.json({ message: "medicine update failed" });
+  }
+};
+
+// delete medicine
+const deleteMedicine = async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const deletedMedicine = await Medicine.findByIdAndDelete(id);
+        if (!deletedMedicine) {
+            return res.json({message: "medicine not found"})
+        }
+
+        res.json({message: "medicine deleted"})
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+
+module.exports = { addMedicine, getMedicine, updateMedicine, deleteMedicine};
--- backEnd/routes/medicineRoutes.js
+++ backEnd/routes/medicineRoutes.js
@@ -1,82 +1,22 @@
 const express = require("express");
-const Medicine = require("../models/medicineModel");
+
+const { addMedicine, getMedicine, updateMedicine, deleteMedicine } = require("../controllers/medicinControllers");
 
 const router = express.Router();
 
 // add new medicine
-router.post("/", async (req, res) => {
-  try {
-    const {
-      name,
-      manufacturer,
-      illnesses,
-      stock,
-      buyPrice,
-      sellPrice,
-      sellerInfo,
-    } = req.body;
-
-    const newMedicine = new Medicine({
-      name,
-      manufacturer,
-      illnesses,
-      stock,
-      buyPrice,
-      sellPrice,
-      sellerInfo,
-    });
-
-    await newMedicine.save();
-    res.status(201).json(newMedicine);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
+router.post("/", addMedicine);
 
 
 // get all medicines
-router.get('/', async(req, res)=>{
-    try {
-        const medicines = await Medicine.find();
-        res.json(medicines)
-    } catch (error) {
-        res.json({message: error.message})
-    }
-})
+router.get('/', getMedicine)
 
 
 //udpate medicine
-router.put("/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const updateData = req.body;
-  
-      const updatedMedicine = await Medicine.findByIdAndUpdate(id, updateData, { new: true });
-  
-      if (!updatedMedicine) {
-        return res.json({ message: "medicine not found" });
-      }
-  
-      res.json(updatedMedicine);
-    } catch (error) {
-      res.json({ message: "medicine update failed" });
-    }
-  });
+router.put("/:id", updateMedicine);
 
 // delete a medicine
-router.delete('/:id', async(req, res)=>{
-    try {
-        const {id} = req.params;
-        const deletedMedicine = await Medicine.findByIdAndDelete(id);
-        if (!deletedMedicine) {
-            return res.json({message: "medicine not found"})
-        }
-
-        res.json({message: "medicine deleted"})
-    } catch (error) {
-        res.json({message: error.message})
-    }
-})
+router.delete('/:id', deleteMedicine )
 
 
 module.exports = router;
