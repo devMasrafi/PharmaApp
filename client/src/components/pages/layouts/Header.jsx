2f4eb4afca326d88d34e64d69965828d6a8e5a53
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-12-27 18:27:48 +0600

    getting user data who logged in

--- client/src/components/pages/layouts/Header.jsx
+++ client/src/components/pages/layouts/Header.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
 
 const Header = () => {
+  const baseUrl = "http://localhost:8000/api/v1/users";
+  const [logedInUser, setLogedInUser] = useState(null);
+
+  const token = Cookies.get("token");
+
+
+  const getUser = async () => {
+    try {
+      const getData = await fetch(`${baseUrl}/profile`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+      });
+
+      if (!getData.ok) {
+        console.log("failed to fetch user");
+      }
+
+      const response = await getData.json();
+      // console.log(response);
+      setLogedInUser(response);
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  // if (logedInUser) {
+  //   console.log(logedInUser.data);
+  // }
+
   return (
     <main>
       <section className="border-b-2 pb-4 border-darkWood rounded-lg ">
         <div className="flex justify-between items-center tracking-wider font-semibold mx-2">
           <div className="font-RaleWayFont text-2xl ">Dashboard</div>
           <div className="flex gap-x-[2rem] items-center ">
-            <h2 className="font-RaleWayFont text-lg capitalize tracking-wider font-normal  ">
-              user name
+            <h2 className="font-interFont text-lg capitalize tracking-wider font-light   ">
+              {
+                logedInUser ? logedInUser.data.username : "Loading..."
+              }
             </h2>
             <button className="px-2 py-1 rounded-full bg-darkWood text-white uppercase">
               o
